7a78ddb48d1adac436a4a7bdfd4df5c78d2c0175
zhangjing <zhangjing@huixiangtech.cn>
2019-03-14 17:48:42 +0800

    1修改样式

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,7 +1,7 @@
 <template>
 	<div id="app">
-		<!-- <router-view></router-view> -->
-		<transition :name="transitionName"><router-view class="Router"></router-view></transition>
+		<router-view></router-view>
+		<!-- <transition :name="transitionName"><router-view class="Router"></router-view></transition> -->
 	</div>
 </template>
 
