ffff145fcf8136256b3dcc2577804f2e98915a67
zhangjing <zhangjing@huixiangtech.cn>
2019-05-24 16:17:04 +0800

    1 日志输出

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,35 +5,48 @@ import App from './App';
 import router from './router';
 import store from './store';
 
-import {ipcRenderer} from 'electron';
+import {
+	ipcRenderer
+} from 'electron';
 import IPC from '#/IPC.js';
 import './assets/css/animate.css';
 import './assets/css/style.css';
 import 'vue2-toast/lib/toast.css';
 import 'swiper/dist/css/swiper.css';
 import Toast from 'vue2-toast';
+// import fs from 'fs';
 Vue.use(Toast);
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'));
-axios.defaults.timeout =  60000;
+axios.defaults.timeout = 60000;
 Vue.http = Vue.prototype.$http = axios;
 Vue.config.productionTip = false;
 /* import ECharts from 'vue-echarts/components/ECharts'
 import 'echarts/lib/chart/line'
 Vue.component('chart', ECharts) */
 new Vue({
-    components: {App},
-    router,
-    store,
-    template: '<App/>'
+	components: {
+		App
+	},
+	router,
+	store,
+	template: '<App/>'
 }).$mount('#app');
 
 ipcRenderer.send(IPC.RENDER_READY);
 
 var devInnerHeight = 1040.0 // 开发时的InnerHeight
 var devInnerWidth = 1920.0 // 开发时的InnerHeight
-var devDevicePixelRatio = 1.0// 开发时的devicepixelratio
-var devScaleFactor = 1.0  // 开发时的ScaleFactor
+var devDevicePixelRatio = 1.0 // 开发时的devicepixelratio
+var devScaleFactor = 1.0 // 开发时的ScaleFactor
 var scaleFactor = require('electron').screen.getPrimaryDisplay().scaleFactor
 //var zoomFactor = (window.innerHeight / devInnerHeight) * (window.devicePixelRatio / devDevicePixelRatio) * (devScaleFactor / scaleFactor);
-var zoomFactor = (window.innerWidth / devInnerWidth) * (window.devicePixelRatio / devDevicePixelRatio) * (devScaleFactor / scaleFactor)
+var zoomFactor = (window.innerWidth / devInnerWidth) * (window.devicePixelRatio / devDevicePixelRatio) * (
+	devScaleFactor / scaleFactor)
 //require('electron').webFrame.setZoomFactor(zoomFactor);
+// 
+//文本输入
+// fs.appendFileSync(
+// 	'message.txt',
+// 	'3e56775656',
+// 	'utf8'
+// );
